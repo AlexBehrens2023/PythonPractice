875ce7b7a75d7d870f960fad6d941e7427d1f2ad
Behrens Alexander <816119@stma.k12.mn.us>
2021-09-13 15:11:55 -0500

    yo

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Squirral are cool.")
+#Print frist video game
+print("Minecaft")
+
+#print 2nd video game
+print("Plants vs Zombies 2")
+
+#print 3nd video game
+print("Jurassic World Alive")
