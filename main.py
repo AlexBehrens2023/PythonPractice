a5b9f3e177fa63d582ff980e44b5a08f7a1d34ac
AlexBehrens2023 <816119@mystma.org>
2021-09-28 14:27:11 -0500

    words

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-def madLid(Ins, Nub, Day, Let, Maj, Nub2, Nub3, Adj, Adj2, Fraction, Adj3, Noun, FM, Song, AdjEd, Art):
+def define(word):
+    word.lower() == word
+    if word == "squirrel":
+        return "The best animal in the world"
+    elif word == "salamango":
+        return "evolved from a Newtgat"
+    elif word == "dragonache":
+        return "a dragon pinata of great power"
+    elif word == "cool":
+        return "someone or something that people think are extonary"
+    elif word == "snake":
+        return "an animal that can eat thing in one bite"
+    elif word == "hate":
+        return "rage that is bilt up overtime"
+    elif word == "sour":
+        return "is a feeling of a lemin in your mouth"
+    elif word == "game thory":
+        return "a youtube that talk about games"
+    elif word == "your mom":
+        return "the preson who gave birth to you"
+    elif word == "shellybean":
+        return "a Sour species that has turned good"
+    else:
+        return "there is no word for that"
 
-    print("While I've stayed at home, I've practiced the " + Ins + " for " + Nub + " hours every " + Day + ". My favorite key signature to play and practice in is " + Let + Maj + ". It has " + Nub + " flats and " + Nub2 + " sharps. I've also gotten awesome at counting time signatures. The time signature I love is " + Fraction + " because it is " + Adj3 + " to count. One instrument I wan to learn how to play while I'm at home is " + Ins + ". , because it is " + Adj + " and sounds like a " + Noun + ". My " + FM + " likes it when I play " + Song + ", and always gibes me a round of applause after my performances! When I return to school, my teacher will be " +AdjEd + " of how great I am at playing my instrument. My teacher might even think I sound like " + Art)
-
-madLid("Trombone", "2", "Friday", "S", "Minor", "15", "67", "big", "Thin", "1/2",  "small", "squirral", "Jon", "Star Spagle Bandor", "bored", "Bob Marley")
-
-print(2 * 5)
+print(define("squirrel"))
