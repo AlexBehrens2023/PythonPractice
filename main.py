e66ac35b1a2de4bc01f68ea05963a3441dd738be
AlexBehrens2023 <816119@mystma.org>
2021-09-29 15:03:09 -0500

    True or False with and

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-def define(word):
-    word.lower() == word
-    if word == "squirrel":
-        return "The best animal in the world"
-    elif word == "salamango":
-        return "evolved from a Newtgat"
-    elif word == "dragonache":
-        return "a dragon pinata of great power"
-    elif word == "cool":
-        return "someone or something that people think are extonary"
-    elif word == "snake":
-        return "an animal that can eat thing in one bite"
-    elif word == "hate":
-        return "rage that is bilt up overtime"
-    elif word == "sour":
-        return "is a feeling of a lemin in your mouth"
-    elif word == "game thory":
-        return "a youtube that talk about games"
-    elif word == "your mom":
-        return "the preson who gave birth to you"
-    elif word == "shellybean":
-        return "a Sour species that has turned good"
-    else:
-        return "there is no word for that"
-
-print(define("squirrel"))
+def function(x, y):
+     if x > 10 and not y > 10:
+            return True
+     else:
+         return False
+print(function(11, 6))
