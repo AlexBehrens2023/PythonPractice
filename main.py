fdd150a9cd20e1d1a62c8c2c733404c97137a8a2
AlexBehrens2023 <816119@mystma.org>
2021-09-30 15:16:29 -0500

    While

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def function(x, y):
-     if x > 10 and not y > 10:
-            return True
-     else:
-         return False
-print(function(11, 6))
+def count():
+
+       x = 1
+
+       while x <= 20 :
+              if x % 2 == 0:
+                     print(x)
+                     print(" if even")
+              else:
+                     print(x)
+                     print(" if odd")
+              x = x + 1
+
+
+count()
